644913383dcba86b3cb134d9173601f9b9d32da1
PhoenixVIP <jazesamor@gmail.com>
2025-01-26 01:07:09 +0530

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,5 +1,35 @@
+
 {
-  "version": "0.01",
-  "link": "https://cdn.discordapp.com/attachments/1330607844864688129/1330930179987607623/libacecore.so?ex=67906d92&is=678f1c12&hm=44279a88d0996b5799e6f9562bb91576dbb0adc19bf16d803a603c24b087b145&",
-  "MyMd5":"MD5here"
+    "libs": [
+        {
+            "name": "libcorex.so",
+            "version": "0.13",
+            "link": "https://cdn.discordapp.com/attachments/1330607844864688129/1332771278968459354/libacecore.so?ex=6796777a&is=679525fa&hm=b50530e96d499753ffe2b8b8480ca28ea545724838a4a7adf530dbd72f477cf9&",
+            "md5": "0C314685EDA09873534DD690F1BBABAF"
+        },
+        {
+            "name": "libnucleus.so",
+            "version": "0.02",
+            "link": "https://cdn.discordapp.com/attachments/1330607844864688129/1331977860960882840/libacecore.so?ex=6793948d&is=6792430d&hm=f786dca8f29f46b8a4ee93df7b954f7f5acba24b9136b08085f27e0ace0a4db6&",
+            "md5": "A7980538E1BD23F863CD9BD0EB410B16"
+        },
+        {
+            "name": "libcoreengine.so",
+            "version": "0.02",
+            "link": "https://cdn.discordapp.com/attachments/1330607844864688129/1331977860960882840/libacecore.so?ex=6793948d&is=6792430d&hm=f786dca8f29f46b8a4ee93df7b954f7f5acba24b9136b08085f27e0ace0a4db6&",
+            "md5": "A7980538E1BD23F863CD9BD0EB410B16"
+        },
+        {
+            "name": "libunifiedcore.so",
+            "version": "0.02",
+            "link": "https://cdn.discordapp.com/attachments/1330607844864688129/1331977860960882840/libacecore.so?ex=6793948d&is=6792430d&hm=f786dca8f29f46b8a4ee93df7b954f7f5acba24b9136b08085f27e0ace0a4db6&",
+            "md5": "A7980538E1BD23F863CD9BD0EB410B16"
+        },
+        {
+            "name": "libcorepulse.so",
+            "version": "0.02",
+            "link": "https://cdn.discordapp.com/attachments/1330607844864688129/1331977860960882840/libacecore.so?ex=6793948d&is=6792430d&hm=f786dca8f29f46b8a4ee93df7b954f7f5acba24b9136b08085f27e0ace0a4db6&",
+            "md5": "A7980538E1BD23F863CD9BD0EB410B16"
+        }
+    ]
 }
